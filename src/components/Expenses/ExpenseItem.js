2177900b73659749df1bfafaf3b1b72290e5f53f
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-07-03 18:45:10 +0530

    Adding_Event_Listerners_made_easy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import ExpenseDate  from './ExpenseDate.js'
 import Card from '../UI/Card';
 const ExpenseItems= (props)=>{
     //let {title , price , locationOfExpenditure} = props;
+    const clickHandler = ()=>{
+      console.log('Deleting expenses: ' , props.title)
+    };
+
     return (
         <div>
               <Card className='expense-item'>
@@ -12,6 +16,7 @@ const ExpenseItems= (props)=>{
             <h2>{props.title}</h2>
             <div className="expense-item__price">{props.amount}</div>
           </div>
+          <button onClick={clickHandler}>Delete Expense</button>
         </Card>
         </div>
       
